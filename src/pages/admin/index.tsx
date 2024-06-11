cb81f8d4b33686ddcf1f23ff97bb68c77efc29db
Jean Lucca <jeanluccabueno@gmail.com>
2024-06-11 12:47:06 -0300

    Registration in the database

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,8 +1,26 @@
-import { useState } from "react";
+import { FormEvent, useState, useEffect } from "react";
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
 
 import { FiTrash } from 'react-icons/fi'
+import { db } from '../../services/firebaseConnection'
+import {
+    addDoc,
+    collection,
+    onSnapshot,
+    query,
+    orderBy,
+    doc,
+    deleteDoc,
+} from 'firebase/firestore'
+
+interface LinkProps{
+  id: string;
+  name: string;
+  url: string;
+  bg: string;
+  color: string;
+}
 
 export function Admin() {
   const [nameInput, setNameInput] = useState("");
@@ -10,11 +28,74 @@ export function Admin() {
   const [textColorInput, setTextColorInput] = useState("#f1f1f1");
   const [backgroundColorInput, setBackgroundColorInput] = useState("#121212");
 
+  const [links, setLinks] = useState<LinkProps[]>([])
+
+  useEffect(() => {
+    const linksRef = collection(db, "links");
+    const queryRef = query(linksRef, orderBy("created", "asc"));
+
+    const unsub = onSnapshot(queryRef, (snapshot) => {
+      let lista = [] as LinkProps[];
+
+      snapshot.forEach((doc) => {
+        lista.push({
+          id: doc.id,
+          name: doc.data().name,
+          url: doc.data().url,
+          bg: doc.data().bg,
+          color: doc.data().color,
+        })
+      })
+
+        setLinks(lista);
+
+    })
+
+    return () => {
+      unsub();
+    }
+
+  }, [])
+
+
+  async function handleRegister(e: FormEvent){
+    e.preventDefault();
+
+    if(nameInput === "" || urlInput === ""){
+      return
+    }
+      
+    addDoc(collection(db, "links"), {
+      name: nameInput,
+      url: urlInput,
+      bg: backgroundColorInput,
+      color: textColorInput,
+      created: new Date()
+    })
+
+    .then(() => {
+      setNameInput("")
+      setUrlInput("")
+      console.log("CADASTRADO COM SUCESSO")
+    })
+
+    .catch((error) => {
+      console.log("ERRO AO CADASTRAR NO BANCO" + error)
+    })
+
+
+  }
+
+  async function handleDeleteLink(id: string){
+    const docRef = doc(db, "links", id)
+    await deleteDoc(docRef)
+  }
+
   return (
     <div className="flex items-center flex-col min-h-screen pb-7 px-2">
       <Header />
 
-      <form className="flex flex-col mt-8 mb-3 w-full max-w-xl">
+      <form className="flex flex-col mt-8 mb-3 w-full max-w-xl" onSubmit={handleRegister}>
         <label className="text-white font-medium mt-2 mb-2">Nome do Link</label>
         <Input
           placeholder="Digite o nome do link...."
@@ -72,16 +153,23 @@ export function Admin() {
           Meus Links
         </h2>
 
-        <article className="flex items-center justify-between w-11/12 max-w-xl rounded py-3 px-2 mb-2 select-none"
-        style={{backgroundColor: "#2563EB", color: "#FFF"}}
+        {links.map((Link) => (
+          <article 
+          key={Link.id}
+          className="flex items-center justify-between w-11/12 max-w-xl rounded py-3 px-2 mb-2 select-none"
+        style={{backgroundColor: Link.bg, color: Link.color}}
         >
-          <p>Canal do Youtube</p>
+          <p>{Link.name}</p>
           <div>
-            <button className="border border-dashed p-1 rounded">
+            <button 
+            className="border border-dashed p-1 rounded"
+            onClick={ () => handleDeleteLink(Link.id)}
+            >  
               <FiTrash size={18} color="#FFFF"/>
             </button>
           </div>
         </article>
+        ))}
 
     </div>
   );
